1c76dbeadb8e4d271e49e3525a0d3ad4a90436ff
mikemehl <michaelalanmehl@gmail.com>
2019-03-09 08:04:39 -0500

    Ok, now using Jinja2 environment. Small refactorings and addition of config parameter for pretty urls. Next step, making links between pages possible.

--- gen_site.py
+++ gen_site.py
@@ -6,35 +6,48 @@ import markdown
 import frontmatter
 import os
 
-def gen_site():
-
+def check_dirs():
   # Check for all required directories and files
   curr_dir = os.listdir() 
   if not "config.yml" in curr_dir:
     print("No config file in directory!")
-    return
+    return False
   if not "layouts" in curr_dir:
     print("No layouts folder in directory!")
+    return False
   if not "pages" in curr_dir:
     print("No pages folder in directory!")
+    return False
+  return True
 
+def read_config():
   # Open the configuration YAML and read in necessary options.
   with open("config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
     site = config["site"] #site variable used across site
+    return config, site
+
+def setup_templates():
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader("layouts"), autoescape=jinja2.select_autoescape(["html", "xml"]))
+  return env
+
+def gen_site():
+
+  if not check_dirs():
+    return
+
+  # Read config options and site variable.
+  config, site = read_config()
 
   # Open templates and have them ready to go
   template_list = os.listdir("layouts")
-  templates = dict() 
-  for template in template_list:
-    with open("layouts/%s" % template, "r") as templ_file:
-      fname, fext = os.path.splitext(template) 
-      templates[fname] = jinja2.Template(templ_file.read())
-  if templates["default"] is None:
+  env = setup_templates()
+  if not "default.html" in env.list_templates():
     print("No default template found!")
     return 
 
   # Now, create our output directory if it doesn't exist.
+  curr_dir = os.listdir()
   if not "output" in curr_dir:
     os.mkdir("output")
 
@@ -50,10 +63,19 @@ def gen_site():
       content = markdown.markdown(content)
       cont_templ = jinja2.Template(content)
       content = cont_templ.render(site=site, page=metadata)
-      out = templates["default"].render(site=site, content=content, page=metadata) 
+      if metadata["layout"]:
+        templ = env.get_template(metadata["layout"] + ".html")
+      else:
+        templ = env.get_template("default.html")
+      out = templ.render(site=site, content=content, page=metadata) 
       name = page.split(".")[0]
-      with open("output/%s%s" % (name, ".html"), "w") as out_file:
-        out_file.write(out)
+      if config["pretty_urls"] and not name == "index":
+        os.mkdir("output/%s" % name)
+        with open("output/%s/index.html" % (name), "w") as out_file:
+          out_file.write(out)
+      else:
+        with open("output/%s%s" % (name, ".html"), "w") as out_file:
+          out_file.write(out)
 
   print("Site generated...")
 
---
+++ pages/about.md
@@ -0,0 +1,14 @@
+---
+title: About 
+layout: default
+---
+
+# About 
+
+It sure is easy to write one of these here fancy static site generators in python.
+
+Why didn't I just do this **before**?
+
+By the way, this site's title is {{ site.title }}.
+
+Pretty nifty huh?
