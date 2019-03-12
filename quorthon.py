6fe888582c32949933f68cc2abb8da1774824901
mikemehl <michaelalanmehl@gmail.com>
2019-03-11 20:15:50 -0400

    Added serve target, but not implemented. Gotta eat!

--- quorthon.py
+++ quorthon.py
@@ -1,5 +1,8 @@
 import click
 import gen_site
+import http.server
+import os
+import log
 
 @click.group()
 def quorthon():
@@ -8,6 +11,10 @@ def quorthon():
 @quorthon.command(help="Build your site to the output directory.")
 def build():
   gen_site.gen_site()
+
+@quorthon.command(help="Serve your site from the output directory.")
+def serve():
+  log.info("NOT YET SUPPORTED")
 
 if __name__ == "__main__":
   quorthon()
