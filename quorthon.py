97327c5aef093aff16639c88fd57c90826ed6927
mikemehl <michaelalanmehl@gmail.com>
2019-03-16 08:53:32 -0400

    Added development server and replaced hardcoded directories with string constants.

--- quorthon.py
+++ quorthon.py
@@ -11,12 +11,25 @@ import markdown
 import sass
 import shutil
 import glob
+import http.server
+import socketserver
+from datetime import datetime
+
+DEFAULT_PORT = 12345
 
 DEFAULT_CONFIG = "---\n\
 site:\n\
   title: \"Test Site\"\n\
-  base_url: \"http://127.0.0.1:8000\"\n\
+  base_url: \"http://127.0.0.1:"+str(DEFAULT_PORT)+"\"\n\
 pretty_urls: True "
+
+OUTPUT_DIR = "output"
+ASSET_DIR = "assets"
+LAYOUT_DIR = "layouts"
+SASS_DIR = "assets/scss"
+PAGE_DIR = "pages"
+DATA_DIR = "data"
+CONFIG_FILE = "config.yml"
 
 def debug(msg):
   out = "[ DEBUG ]: %s" % msg
@@ -45,7 +58,7 @@ class PageData():
     self.name = os.path.splitext(name)[0]
 
   def _setup_url(self):
-    rel_name = os.path.relpath(self.fname, "pages")  
+    rel_name = os.path.relpath(self.fname, PAGE_DIR)  
     url = os.path.splitext(rel_name)[0]
     if not PageData.pretty:
       url += ".html" 
@@ -63,6 +76,8 @@ class PageData():
       self.metadata = metadata
       self.content = content
       self.file_ext = os.path.splitext(self.fname)[1]
+      if not "date" in metadata:
+        self.metadata["date"] = datetime.now()
 
   def __repr__(self):
     outstr  = "===============\n"
@@ -77,20 +92,20 @@ class PageData():
 def check_dirs():
   # Check for all required directories and files
   curr_dir = os.listdir() 
-  if not "config.yml" in curr_dir:
-    print("No config file in directory!")
+  if not CONFIG_FILE in curr_dir:
+    info("No config file in directory!")
     return False
-  if not "layouts" in curr_dir:
-    print("No layouts folder in directory!")
+  if not LAYOUT_DIR in curr_dir:
+    info("No layouts folder in directory!")
     return False
-  if not "pages" in curr_dir:
-    print("No pages folder in directory!")
+  if not PAGE_DIR in curr_dir:
+    info("No pages folder in directory!")
     return False
   return True
 
 def read_config():
   # Open the configuration YAML and read in necessary options.
-  with open("config.yml", "r") as config_file:
+  with open(CONFIG_FILE, "r") as config_file:
     config = yaml.safe_load(config_file)
     site = config["site"] #site variable used across site
     if "pretty_urls" in config:
@@ -98,9 +113,9 @@ def read_config():
     return config, site
 
 def parse_data():
-   if not os.path.isdir("data"):
+   if not os.path.isdir(DATA_DIR):
      return None 
-   yaml_files = glob.glob("data/*.yml")
+   yaml_files = glob.glob(DATA_DIR + "/*.yml")
    data = dict()
    for data_file in yaml_files:
      name = os.path.basename(data_file)
@@ -110,13 +125,12 @@ def parse_data():
    return data
        
 def setup_templates():
-  env = jinja2.Environment(loader=jinja2.FileSystemLoader("layouts"))
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader(LAYOUT_DIR))
   return env
 
 def parse_pages():
-  page_dir = "pages"
   pages_info = []
-  for _dir, subdir, files, in os.walk(page_dir):
+  for _dir, subdir, files, in os.walk(PAGE_DIR):
     for file_name in files:
       fname = os.path.join(_dir, file_name)
       page = PageData(fname)
@@ -138,11 +152,11 @@ def gen_page(page, site, config, env):
       error("Template %s not found!" % page.metadata["layout"])
     out = templ.render(site=site, content=content, page=page.metadata)
     if PageData.pretty:
-      outdir = os.path.join(os.getcwd(), "output")
+      outdir = os.path.join(os.getcwd(), OUTPUT_DIR)
       outdir = os.path.join(outdir, page.url[1:])
       if not os.path.isdir(outdir):
         os.makedirs(outdir)
-      with open("output/%s/index.html" % page.url, "w") as f:
+      with open(OUTPUT_DIR+"/%s/index.html" % page.url, "w") as f:
         f.write(out)
     else:     
       warning("UGLY URLS NOT YET SUPPORTED")
@@ -151,31 +165,31 @@ def gen_page(page, site, config, env):
   return
 
 def compile_sass():
-  IN_DIR = "output/assets/scss"
-  OUT_DIR = "output/assets/css"
+  IN_DIR = OUTPUT_DIR+SASS_DIR
+  OUT_DIR = OUTPUT_DIR+ "/" + ASSET_DIR + "/css"
   if not os.path.isdir(IN_DIR):
     return
   if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
-  sass.compile(dirname=("assets/scss", "assets/css"))
+  sass.compile(dirname=(SASS_DIR, ASSET_DIR + "/css"))
   shutil.rmtree(IN_DIR)
   return
 
 def init_dirs():
   curr_dir = os.listdir() 
-  if not "config.yml" in curr_dir:
-    print("Creating config.yml...")
-    with open("config.yml", "w") as config_yml:
+  if not CONFIG_FILE in curr_dir:
+    info("Creating config.yml...")
+    with open(CONFIG_FILE, "w") as config_yml:
       config_yml.write(DEFAULT_CONFIG)
-  if not "layouts" in curr_dir:
-    print("Creating layouts directory...")
-    os.mkdir("layouts")
-  if not "pages" in curr_dir:
-    print("Creating pages directory...")
-    os.mkdir("pages")
-  if not "assets" in curr_dir:
-    print("Creating assets directory...")
-    os.mkdir("assets")
+  if not LAYOUT_DIR in curr_dir:
+    info("Creating layouts directory...")
+    os.mkdir(LAYOUT_DIR)
+  if not PAGE_DIR in curr_dir:
+    info("Creating pages directory...")
+    os.mkdir(PAGE_DIR)
+  if not ASSET_DIR in curr_dir:
+    info("Creating assets directory...")
+    os.mkdir(ASSET_DIR)
 
 
 def gen_site():
@@ -187,10 +201,10 @@ def gen_site():
   config, site = read_config()
 
   # Open templates and have them ready to go
-  template_list = os.listdir("layouts")
+  template_list = os.listdir(LAYOUT_DIR)
   env = setup_templates()
   if not "default.html" in env.list_templates():
-    print("No default template found!")
+    info("No default template found!")
     return 
 
   # Load all data into our site variable
@@ -198,12 +212,12 @@ def gen_site():
   site["data"] = data
 
   # Now, create our output directory.
-  if "output" in os.listdir():
-    shutil.rmtree("output")
-  os.mkdir("output")
+  if OUTPUT_DIR in os.listdir():
+    shutil.rmtree(OUTPUT_DIR)
+  os.mkdir(OUTPUT_DIR)
 
   # Copy over files in the assets directory
-  shutil.copytree("assets", "output/assets")
+  shutil.copytree(ASSET_DIR, OUTPUT_DIR + "/" + ASSET_DIR)
 
   # Compile any SASS found.
   compile_sass()
@@ -212,8 +226,20 @@ def gen_site():
   pages = parse_pages()
   for page in pages:
     gen_page(page, site, config, env)
-  print("Site generated...")
+  info("Site generated...")
 
+def run_dev_server(port):
+  if not os.path.isdir(OUTPUT_DIR):
+    error("No build directory found!")
+  os.chdir(OUTPUT_DIR)
+  handler = http.server.SimpleHTTPRequestHandler
+  httpd = socketserver.TCPServer(("", port), handler)
+  try:
+    info("Starting server at port %d" % port)
+    httpd.serve_forever()
+  finally:
+    httpd.server_close() 
+  pass
 
 @click.group()
 def quorthon():
@@ -224,10 +250,13 @@ def build():
   gen_site()
 
 @quorthon.command(help="Serve your site from the output directory.")
-def serve():
-  info("INIT: NOT YET SUPPORTED")
+@click.option('-p', nargs=1, type=int)
+def serve(p):
+  if p:
+    run_dev_server(p)
+  run_dev_server(DEFAULT_PORT)
 
-@quorthon.command(help="Initialize directory/file structure for Quorthon.")
+@quorthon.command(help="Initialize directory/file structure.")
 def init():
   init_dirs()
 
